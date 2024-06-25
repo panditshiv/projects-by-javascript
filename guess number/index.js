d93445e9f62a00298c4ba29f5a300a2469024c8e
Shiv Shankar <127024673+panditshiv@users.noreply.github.com>
2024-06-25 14:23:16 +0530

    Add files via upload

---
+++ guess number/index.js
@@ -0,0 +1,26 @@
+// const score =400
+// console.log(score)
+//
+// const balance = new Number(100)
+// console.log(balance)
+//
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(4))
+// const otherNumber =23.8966
+// console.log(otherNumber.toPrecision(4))
+// console.log(otherNumber.)
+
+
+// console.log(Math)
+// console.log(Math.abs(-6))
+// console.log(Math.round(8.99))
+// console.log(Math.ceil(8.4))
+// console.log(Math.floor(8.5))
+// console.log(Math.max(8,3,5,6,9))
+// console.log(Math.min(8,3,5,6,9))
+
+// console.log(Math.random())
+//
+// console.log(Math.random()*10)
+// console.log(Math.random()*100)
+// console.log(Math.floor(Math.random()*(1000)+10))
