204aaf5ee3298d143af828900bd045884a99c3f3
Shiv Shankar <127024673+panditshiv@users.noreply.github.com>
2024-06-25 14:24:37 +0530

    Add files via upload

---
+++ ussiness name Generator/index.js
@@ -0,0 +1,39 @@
+//Business name Generator
+
+//
+// let rand=Math.random()
+// let first,second,third;
+//
+//
+// if(rand<0.33){
+//     first='crazy'
+// }
+// else if(rand<0.66 && rand>=0.33){
+//     first='Amazing'
+// }
+// else{
+//     first='Prashid'
+// }
+// rand=Math.random()
+// if(rand<0.33){
+//     second='Engine'
+// }
+// else if(rand<0.66 && rand>=0.33){
+//     second='Brajwasi'
+// }
+// else{
+//     second='Garments'
+// }
+// rand=Math.random()
+// if(rand<0.33){
+//     third='hub'
+// }
+// else if(rand<0.66 && rand>=0.33){
+//     third='wala'
+// }
+// else{
+//     third='stylish'
+// }
+//
+// console.log(`${first} ${second} ${third} `)
+
